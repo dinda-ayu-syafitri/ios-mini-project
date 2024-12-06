76e7c283805165a2f2c2a438fac7e124c2eaf23c
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2024-12-06 23:47:02 +0700

    Add: .gitignore

--- ios-mini-project/FirstScreen.swift
+++ ios-mini-project/FirstScreen.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class FirstScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemMint
         // Do any additional setup after loading the view.
     }
-
-
 }
-
