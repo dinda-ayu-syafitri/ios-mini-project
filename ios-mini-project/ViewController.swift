9d7dc880a6b03900720f653d5a7ffd542c7bf4e7
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2024-12-06 12:10:03 +0700

    Initial Commit

---
+++ ios-mini-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-mini-project
+//
+//  Created by Dinda Ayu Syafitri on 06/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
